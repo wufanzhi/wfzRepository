0b1f19532913e38b2027ff6b1f9aa987febd56b9
wufanzhi_v <wufanzhi_v>
2020-08-18 16:03:04 +0800

    测试fetch和pull

--- src/main/java/com/test/wfz/controller/TestController.java
+++ src/main/java/com/test/wfz/controller/TestController.java
@@ -5,6 +5,7 @@ public class TestController {
     public String testMessage () {
         String s = "6543";
         System.out.println(s);
-        return s;
+        String[] sArr = s.split("4");
+        return sArr[0];
     }
 }

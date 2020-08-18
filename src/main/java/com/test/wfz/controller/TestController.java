66e89608541f27d2a192a014f31295165d697f27
wufanzhi_v <wufanzhi_v>
2020-08-18 15:25:58 +0800

    test提交

---
+++ src/main/java/com/test/wfz/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.test.wfz.controller;
+
+public class TestController {
+
+    public String testMessage () {
+        String s = "6543";
+        System.out.println(s);
+        return s;
+    }
+}

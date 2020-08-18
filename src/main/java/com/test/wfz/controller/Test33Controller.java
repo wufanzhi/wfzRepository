d01b2469836e405bcbc59dc8f6514b183a787742
wufanzhi_v <wufanzhi_v>
2020-08-18 14:25:54 +0800

    33

---
+++ src/main/java/com/test/wfz/controller/Test33Controller.java
@@ -0,0 +1,9 @@
+package com.test.wfz.controller;
+
+public class Test33Controller {
+    public String testMessage() {
+        String s = "3213344";
+        System.out.println(s);
+        return s;
+    }
+}

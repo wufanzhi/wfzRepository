2370a4bb9c2ab4916a81ad710c6749611f0be020
wufanzhi_v <wufanzhi_v>
2020-08-21 15:24:33 +0800

    版本回退

--- src/main/java/com/test/wfz/controller/TestController.java
+++ src/main/java/com/test/wfz/controller/TestController.java
@@ -1,10 +1,25 @@
 package com.test.wfz.controller;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestController {
 
     public String testMessage () {
         String s = "65143";
         System.out.println(s);
         return s;
+    }
+
+    public static void main(String[] args) {
+        BigDecimal d = new BigDecimal(0.00);
+        List<Double> list = new ArrayList<>();
+        list.add(1.1d);
+        list.forEach(e ->{
+            d.add(new BigDecimal(e));
+        });
+        d = d.add(new BigDecimal(1.1d));
+        System.out.println(d.doubleValue());
     }
 }

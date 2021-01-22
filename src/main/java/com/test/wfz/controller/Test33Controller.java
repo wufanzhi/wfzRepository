26b025ca1be8e44df5ca96d4a800a70444edeac2
wufanzhi_v <wufanzhi_v>
2021-01-22 14:06:02 +0800

    321

--- src/main/java/com/test/wfz/controller/Test33Controller.java
+++ src/main/java/com/test/wfz/controller/Test33Controller.java
@@ -3,6 +3,7 @@ package com.test.wfz.controller;
 public class Test33Controller {
     public String testMessage() {
         String s = "3213344";
+        s = "76543";
         System.out.println(s);
         return s;
     }
